8c76a3c3d794b3308e60784d3392c81b34905f04
Agustin Morere <amorere10@gmail.com>
2022-11-22 16:10:41 -0300

    ejercico de Mytasks, leer, editar, agregar

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/new', to: 'tasks#new', as: :new
+  get 'tasks/:id', to: 'tasks#show', as: :show
+  post 'tasks', to: 'tasks#create', as: :create
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+  patch 'tasks/:id', to: 'tasks#update', as: :task
+  delete 'tasks/:id', to: 'tasks#destroy', as: :destroy
 end
